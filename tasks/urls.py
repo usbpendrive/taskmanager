a8ad47b946f93c3724c6b1e11b7d449ef750c6d4
usbpendrive <ferry.fernando@gmail.com>
2025-12-04 14:57:45 +0700

    Fix tag error

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='tasks/home.html', name='home'), name='home'),
+    path('', TemplateView.as_view(template_name='tasks/home.html'), name='home'),
     path('help/', TemplateView.as_view(template_name='tasks/help.html'), name='help'),
 ]
