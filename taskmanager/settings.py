f1ba25797744865764e20ee28190c4e6e5bdf271
usbpendrive <ferry.fernando@gmail.com>
2025-12-19 13:51:43 +0700

    Add base html layout

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'tasks',
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'taskmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
